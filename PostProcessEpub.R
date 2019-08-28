783f2f670b7cf2a5eaa83f623ca917fabf3bcd7d
schuemie <schuemie@ohdsi.org>
2019-08-28 16:58:28 +0200

    Centering EPUB cover image

--- PostProcessEpub.R
+++ PostProcessEpub.R
@@ -1,12 +1,17 @@
+writeLines("Post-processing Epub")
+
 if (system.file(package = "DatabaseConnector") == '') install.packages("DatabaseConnector")
 
+# Unpackage Epub -----------------------------------------------------------------------------------
+writeLines("- Unpackaging Epub")
 tempFolder <- tempfile()
 unzip(file.path("_book", "TheBookOfOhdsi.epub"), exdir = tempFolder)
 
+# Remove dead links -----------------------------------------------------------------------------------
 htmlFiles <- list.files(path = file.path(tempFolder, "EPUB", "text"), pattern = ".*xhtml", full.names = TRUE)
 
 removeDeadLinks <- function(htmlFile) {
-  print(htmlFile)
+  writeLines(paste("-Dropping dead links in", htmlFile))
   html <- readChar(htmlFile, file.info(htmlFile)$size)
   matches <- gregexpr("<a href=\"#[^\"]*\">[^<]+</a>", html)
   links <- regmatches(html, matches)[[1]]
@@ -21,9 +26,17 @@ removeDeadLinks <- function(htmlFile) {
     sink()
   }
 }
-lapply(htmlFiles, removeDeadLinks)
+dummy <- lapply(htmlFiles, removeDeadLinks)
 
+# Center cover image ---------------------------------------------------------------------------------
+writeLines("- Centering cover image")
+unlink(file.path(tempFolder, "EPUB", "text", "cover.xhtml"))
+file.copy(file.path("extras", "Cover", "epubCover.xhtml"), file.path(tempFolder, "EPUB", "text", "cover.xhtml"))
+
+# Repackage Epub -----------------------------------------------------------------------------------
+writeLines("- Repackaging Epub")
 DatabaseConnector::createZipFile(zipFile = file.path("_book", "TheBookOfOhdsi.epub"),
                                  files = tempFolder,
                                  rootFolder = tempFolder)
 unlink(tempFolder)
+writeLines("Done post-processing Epub")
